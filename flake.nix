52d0458ee531912d392d45977db4338ca2b734f9
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-23 22:56:56 +0200

    update the urls to flakehub

--- flake.nix
+++ flake.nix
@@ -3,15 +3,15 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*.tar.gz";
-    nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "https://flakehub.com/f/nix-community/home-manager/0.2405.*.tar.gz";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     alejandra.url = "https://flakehub.com/f/kamadorueda/alejandra/3.*.tar.gz";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
 
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.url = "https://flakehub.com/f/Mic92/sops-nix/0.1.*.tar.gz";
     sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
