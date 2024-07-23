a897076c5290f2a23cdcac97974eb2dba9ae7b2b
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-24 00:34:19 +0200

    remove autologin

--- hosts/nyx/default.nix
+++ hosts/nyx/default.nix
@@ -69,14 +69,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "alexfrede";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
   # Install firefox.
   programs.firefox.enable = true;
 
