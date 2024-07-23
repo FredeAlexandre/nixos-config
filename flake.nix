b5437f64de7f8c472049bd536b60e0671492968c
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-23 22:44:41 +0200

    add sops to the configuration

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
 
     alejandra.url = "https://flakehub.com/f/kamadorueda/alejandra/3.*.tar.gz";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = {
