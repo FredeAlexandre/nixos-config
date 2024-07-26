9ce4e887e1c5296bb94dcb1340a8eec59a21ca1f
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-07-26 21:02:11 +0200

    update the config theme

--- hosts/common/dotfiles/dwm/config.def.h
+++ hosts/common/dotfiles/dwm/config.def.h
@@ -8,15 +8,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "FiraCode Nerd Font:size=12:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char background[]      = "#282828";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char primary[]         = "#458588";
+static const char accent[]          = "#83A598";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, background, accent },
+	[SchemeSel]  = { col_gray4, primary,  primary  },
 };
 
 /* tagging */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", background, "-nf", col_gray3, "-sb", primary, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
