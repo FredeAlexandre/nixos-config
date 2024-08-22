6e8aa99159607a49cfff9c59fa1a3856c72e9546
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-08-22 19:53:44 +0200

    make pkgs folder easier

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,1 @@
-# You can build these directly using 'nix build .#example'
-{pkgs ? import <nixpkgs> {}}: rec {
-  #################### Packages with external source ####################
-
-  bluetui = pkgs.callPackage ./bluetui.nix {};
-}
+{pkgs}: {}
