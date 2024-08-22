0c8c55f143585c63f857f4c6acaa10f4dae28eb1
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-08-22 20:38:01 +0200

    move to plasma desktop environement

--- configs/desktop-environement.nix
+++ configs/desktop-environement.nix
@@ -1,13 +1,9 @@
 {...}: {
-  # Enable the X11 windowing system
   services.xserver = {
     enable = true;
-
-    # Hidpi screen
     dpi = 180;
-
-    # Xfce desktop manager
-    displayManager.sddm.enable = true;
-    desktopManager.plasma6.enable = true;
   };
+
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 }
